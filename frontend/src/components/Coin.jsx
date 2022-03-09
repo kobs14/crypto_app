628e9572a3834b1a83935bf8d03dc5f55b5b1c53
kobi-khor <57593714+kobi-khor@users.noreply.github.com>
2022-03-09 16:04:30 +0200

    Home page layout and  other UI fixed. News.jsx -> still need to be modified for screen size changes

--- frontend/src/components/Coin.jsx
+++ frontend/src/components/Coin.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Coin.css';
+import millify from 'millify';
 
 const Coin = ({
   name,
@@ -11,6 +12,29 @@ const Coin = ({
   priceChange
 }) => {
   return (
+    // <div className='coin-container'>
+    //   <div className='coin-row'>
+    //     <div className='coin text-white'>
+    //       <img src={image} alt='crypto' />
+    //       <h1>{name}</h1>
+    //       <p className='coin-symbol'>{symbol}</p>
+    //     </div>
+    //     <div className='coin-data text-white'>
+    //       <p className='coin-price'>${Number(price).toFixed(2)}</p>
+    //       <p className='coin-volume'>${volume?.toLocaleString()}</p>
+
+    //       {priceChange < 0 ? (
+    //         <p className='coin-percent red'>{Number(priceChange).toFixed(2)}%</p>
+    //       ) : (
+    //         <p className='coin-percent green'>{Number(priceChange).toFixed(2)}%</p>
+    //       )}
+
+    //       <p className='coin-marketcap'>
+    //         Mkt Cap: ${marketcap.toLocaleString()}
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
     <div className='coin-container'>
       <div className='coin-row'>
         <div className='coin text-white'>
@@ -19,8 +43,8 @@ const Coin = ({
           <p className='coin-symbol'>{symbol}</p>
         </div>
         <div className='coin-data text-white'>
-          <p className='coin-price'>${Number(price).toFixed(3)}</p>
-          <p className='coin-volume'>${volume?.toLocaleString()}</p>
+          <p className='coin-price'>${Number(price).toFixed(2)}</p>
+          <p className='coin-volume'>${millify(volume?.toLocaleString())}</p>
 
           {priceChange < 0 ? (
             <p className='coin-percent red'>{Number(priceChange).toFixed(2)}%</p>
@@ -29,11 +53,12 @@ const Coin = ({
           )}
 
           <p className='coin-marketcap'>
-            Mkt Cap: ${marketcap.toLocaleString()}
+            Mkt Cap: ${millify(marketcap.toLocaleString())}
           </p>
         </div>
       </div>
     </div>
+    
   );
 };
 
