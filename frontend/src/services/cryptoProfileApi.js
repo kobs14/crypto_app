0dffb07eff505deaa503684b726e9ceea7112fc2
kobi-khor <57593714+kobi-khor@users.noreply.github.com>
2022-05-12 11:16:44 +0300

    fixed bugs, Dockerize, ready for deploy

--- frontend/src/services/cryptoProfileApi.js
+++ frontend/src/services/cryptoProfileApi.js
@@ -4,7 +4,7 @@ const cryptoNewsHeaders = {
     'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJCb24tSm92aSIsImlhdCI6MTY0NDk0MDcyNCwiZXhwIjoxNjQ1MDI3MTI0fQ.ZVudaEo4QCmeoP9pgHTvFdiv6C4l0uu72efVWj6h-CatgORbD3uoLsBsuxNPe1g6NFlSd4TP8rxNLDlnUG1eqQ',
 }
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'http://54.237.207.128:8080';
 
 const  createRequest = (url) => ({url, headers: cryptoNewsHeaders});
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     // generate manifest.json in outDir
+   // host: '0.0.0.0',
     manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: './main.jsx'
-    }
+    // rollupOptions: {
+    //   // overwrite default .html entry
+    //   input: './main.jsx'
+    // }
   }
 })
